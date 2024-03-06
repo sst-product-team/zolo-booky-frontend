2fb871c4db992bbd876324301a965e32a6d9d710
Kushagra <kushagra.rigel@gmail.com>
2024-03-06 09:44:41 +0530

    Added global user context

--- app/src/main/java/com/example/test/Constants.kt
+++ app/src/main/java/com/example/test/Constants.kt
@@ -1,6 +1,8 @@
 package com.example.test
 
 object Constants {
-    const val BASE_URL = "https://api-zolo.onrender.com"
-    // Add other constants as needed
+    const val BASE_URL = "http://192.168.67.207:8080"
+    var USER_ID = -1
+    var USER_NAME = ""
+    var USER_FCM = ""
 }
