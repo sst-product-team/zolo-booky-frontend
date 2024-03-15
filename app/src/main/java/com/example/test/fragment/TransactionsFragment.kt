12a2650ba0be73ce3ac85d8fa2319052d858c86e
pradyutf <pradyutfogla@gmail.com>
2024-03-15 22:41:58 +0530

    implemented tab nav for transactions

--- app/src/main/java/com/example/test/fragment/TransactionsFragment.kt
+++ app/src/main/java/com/example/test/fragment/TransactionsFragment.kt
@@ -5,29 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.test.R
+import com.example.test.adapter.TabAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TransactionsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TransactionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var tabAdapter: TabAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find the TabLayout and ViewPager2 within the inflated layout
+        tabLayout = view.findViewById(R.id.tabLayout)
+        viewPager2 = view.findViewById(R.id.viewPager2)
+
+
+        tabAdapter= TabAdapter( childFragmentManager,lifecycle)
+
+
+        tabLayout.addTab(tabLayout.newTab().setText("Borrowed"))
+        tabLayout.addTab(tabLayout.newTab().setText("Requests"))
+
+
+        viewPager2.adapter = tabAdapter
+
+        tabLayout.addOnTabSelectedListener(object :OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+
+        viewPager2.registerOnPageChangeCallback(object :OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+
+
+        // Now you can work with tabLayout and viewPager2
     }
 
     override fun onCreateView(
@@ -39,22 +77,6 @@ class TransactionsFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TransactionsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TransactionsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
