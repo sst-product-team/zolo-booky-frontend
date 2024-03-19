b86efbe5a44699900eba9fc8782216cedf3ca717
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-03-19 13:03:40 +0530

    request adapter error resolved

--- app/src/main/java/com/example/test/tabs/TabRequests.kt
+++ app/src/main/java/com/example/test/tabs/TabRequests.kt
@@ -2,82 +2,41 @@ package com.example.test.tabs
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
-
 import com.example.test.R
-import com.example.test.adapter.BookBorrowAdapter
 import com.example.test.adapter.BookRequestsAdapter
 import com.example.test.databinding.FragmentTabRequestsBinding
 import com.example.test.entity.ListBookEntity
 import com.example.test.globalContexts.Constants
 
-class TabRequests : Fragment() {
+class TabRequests() : Fragment() {
     private lateinit var binding: FragmentTabRequestsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.BookRecyclerView)
-        val queue = Volley.newRequestQueue(requireContext())
-
-        val url = "${Constants.BASE_URL}/v0/books?size=100"
-
-
-        Log.d("API Request URL", url)
-
-        val jsonArrayRequest = JsonArrayRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                Log.d("API Response", response.toString())
-                val books = mutableListOf<ListBookEntity>()
-                for (i in 0 until response.length()) {
-                    val bookObject = response.getJSONObject(i)
-
-                    val bookId = bookObject.getInt("id")
-                    val bookTitle = bookObject.getString("name")
-                    val bookStatus = bookObject.getString("status")
-
-                    if(bookStatus=="AVAILABLE"){
-                        books.add(
-                            ListBookEntity(bookId, bookTitle, bookStatus)
-                        )}
-                }
-                Log.d("Parsed Books", "Number of books fetched: ${books.size}")
-
-
-                val adapter = BookListAdapter(books)
-                recyclerView.layoutManager = LinearLayoutManager(requireContext())
-                recyclerView.adapter = adapter
-                adapter.notifyDataSetChanged() // Ensures the adapter knows the data has changed
-
-            },
-            { error ->
-                Log.e("API Error", error.toString())
-                Log.e("VolleyExample", "Error: $error")
-            }
-        )
-
-        queue.add(jsonArrayRequest)
+    override fun onCreateView(
+        inflater: android.view.LayoutInflater, container: android.view.ViewGroup?,
+        savedInstanceState: android.os.Bundle?
+    ): android.view.View? {
+        binding = FragmentTabRequestsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = binding.rvRequestsBookList
 
+        val recyclerView = binding.rvRequestsBookList
         val queue = Volley.newRequestQueue(requireContext())
 
         val url = "${Constants.BASE_URL}/v0/books?size=100"
-
-        var token: String
 
         Log.d("API Request URL", url)
 
@@ -97,7 +56,6 @@ class TabRequests : Fragment() {
                 }
                 Log.d("Parsed Books", "Number of books fetched: ${books.size}")
 
-
                 val adapter = BookRequestsAdapter(requireContext(), books)
                 recyclerView.layoutManager = LinearLayoutManager(requireContext())
                 recyclerView.adapter = adapter
@@ -115,4 +73,5 @@ class TabRequests : Fragment() {
 
     companion object {
     }
+
 }
