9815ee1f63f87ed2360569aba6f89bbf1f2f3366
Kushagra <kushagra.rigel@gmail.com>
2024-03-21 13:34:41 +0530

    Added additional functionality for requests and borrowed books.
    Signed-off-by: Kushagra <kushagra.rigel@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.6.2")
     implementation("com.squareup.retrofit:converter-gson:2.0.0-beta2")
     implementation("com.android.volley:volley:1.2.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
--- app/src/main/java/com/example/test/adapter/BookBorrowAdapter.kt
+++ app/src/main/java/com/example/test/adapter/BookBorrowAdapter.kt
@@ -1,25 +1,32 @@
 package com.example.test.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.example.test.R
 import com.example.test.databinding.BooklistBinding
 import com.example.test.entity.ListAppealEntity
-import com.example.test.entity.ListBookEntity
+import com.example.test.globalContexts.Constants
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
+import org.json.JSONObject
 
 class BookBorrowAdapter(private val context: Context, private val books: List<ListAppealEntity>) : RecyclerView.Adapter<BookBorrowAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = BooklistBinding.bind(view)
     }
+
+    var queue = Volley.newRequestQueue(context)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.booklist, parent, false)
@@ -44,13 +51,14 @@ class BookBorrowAdapter(private val context: Context, private val books: List<Li
 
         } else {
             holder.itemView.setOnClickListener {
-                showCustomDialog()
+                showCustomDialog(book)
             }
         }
     }
 
-    private fun showCustomDialog() {
-        val dialogView = LayoutInflater.from(context).inflate(R.layout.bottomsheet_conformation, null)
+    private fun showCustomDialog(appeal: ListAppealEntity) {
+        val dialogView =
+            LayoutInflater.from(context).inflate(R.layout.bottomsheet_conformation, null)
         val dialog = BottomSheetDialog(context)
         dialog.setContentView(dialogView)
 
@@ -67,10 +75,30 @@ class BookBorrowAdapter(private val context: Context, private val books: List<Li
         }
 
         btnConfirm.setOnClickListener {
-            dialog.dismiss()
+            returnBook(appeal)
         }
 
         dialog.show()
+    }
+
+    public fun returnBook(appeal: ListAppealEntity) {
+        val url = "${Constants.BASE_URL}/v0/appeals/${appeal.trans_id}"
+        val newStatus = JSONObject().apply {
+            put("trans_status", "COMPLETED")
+        }
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.PATCH, url, newStatus,
+            { response ->
+                Log.d("APPEAL UPDATE", "APPEAL UPDATED")
+                Toast.makeText(context, "Book returned successfully", Toast.LENGTH_SHORT).show()
+            },
+            { error ->
+                Log.d("APPEAL UPDATE", "ERROR OCCURED")
+            }
+        )
+        queue.add(jsonObjectRequest)
+
     }
 
     override fun getItemCount() = books.size
